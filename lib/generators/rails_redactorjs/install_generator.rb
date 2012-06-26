a0a6dbbc6e315b8fa76f984e9bf295ccccfb54d3
Sammy <sammylintw@gmail.com>
2012-06-27 01:42:11 +0800

    move repo

---
+++ lib/generators/rails_redactorjs/install_generator.rb
@@ -0,0 +1,16 @@
+#require 'rails/generators'
+module RailsRedactorjs
+  module Generators
+    class InstallGenerator < ::Rails::Generators::Base
+  #    source_root File.expand_path('../templates', __FILE__)
+  #    include Rails::Generators::Migration
+#      def install
+#	template "base/carrierwave/uploaders/redactor_attachment_file_uploader.rb",
+#	  File.join("app/uploaders", "redactor_attachment_file_uploader.rb")
+
+#	template "base/carrierwave/uploaders/redactor_picture_uploader.rb",
+#	  File.join("app/uploaders", "redactor_picture_uploader.rb")
+#      end
+    end
+  end
+end
